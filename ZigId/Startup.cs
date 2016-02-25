34c3f3e6ba5d9cd315d7057b370896058ad50163
djechelon <djechelon@github.com>
2016-02-25 17:41:51 +0100

    Commit the layout

---
+++ ZigId/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZigId.Startup))]
+namespace ZigId
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
