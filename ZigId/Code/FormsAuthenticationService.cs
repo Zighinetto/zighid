782c8eeee1ed1304590bab0732761ba644db036c
djechelon <djechelon@github.com>
2016-02-29 16:44:04 +0100

    Copied from sample

---
+++ ZigId/Code/FormsAuthenticationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace ZigId.Code
+{
+
+    public class FormsAuthenticationService : IFormsAuthentication
+    {
+        public string SignedInUsername
+        {
+            get { return HttpContext.Current.User.Identity.Name; }
+        }
+
+        public DateTime? SignedInTimestampUtc
+        {
+            get
+            {
+                var cookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (cookie != null)
+                {
+                    var ticket = FormsAuthentication.Decrypt(cookie.Value);
+                    return ticket.IssueDate.ToUniversalTime();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void SignIn(string userName, bool createPersistentCookie)
+        {
+            FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
+        }
+
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
